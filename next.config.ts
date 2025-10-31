70b772aae0110d49075dff53cc1a6d5636b69087
simonliu <simonliuoffer001@gmail.com>
2025-10-31 19:34:49 -0400

    git: fix deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   distDir: "dist",
+  basePath: isProd ? 'https://sinanliu001.github.io' : '',
+  assetPrefix: isProd ? 'https://sinanliu001.github.io' : '',
   images: {
     unoptimized: true,
   },
