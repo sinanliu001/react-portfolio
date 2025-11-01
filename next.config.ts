147c92786ac245a5081a61abf6e528a45810fc58
simonliu <simonliuoffer001@gmail.com>
2025-10-31 22:02:01 -0400

    fix: update config

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   /* config options here */
-  basePath: "",
+  basePath: isProd ? "/react-portfolio" : "",
   output: "export",
   reactStrictMode: true,
 };
