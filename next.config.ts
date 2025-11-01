0ceadbea75289c355ecd4b2c18cff5d5df18dbcf
simonliu <simonliuoffer001@gmail.com>
2025-10-31 21:56:35 -0400

    fix: update config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-// const isProd = process.env.NODE_ENV === "production";
-// todo: change basePath when deploying to production
-
-const nextConfig: NextConfig = {
+// // const isProd = process.env.NODE_ENV === "production";
+// // todo: change basePath when deploying to production
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   basePath: "/react-portfolio",
   output: "export",
   reactStrictMode: true,
 };
 
-export default nextConfig;
+// export default nextConfig;
+module.exports = nextConfig;
